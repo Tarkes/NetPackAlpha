70151be55f95fd97699d84475bc61b5ca389b5e0
Tarkes <dani-san@chello.at>
2014-02-06 14:03:20 +0100

    Second Commit, Initial Files

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace NetPackAlpha
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Reines Gerüst
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
