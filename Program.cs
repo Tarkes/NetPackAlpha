75f540fca61909f4c0bd7082c055357532c1c4f5
Tarkes <dani-san@chello.at>
2014-02-09 20:08:42 +0100

    third commit, some steps forward

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net.Sockets;
 
 namespace NetPackAlpha
 {
@@ -6,8 +7,24 @@ namespace NetPackAlpha
 	{
 		public static void Main (string[] args)
 		{
-			//Reines Gerüst
-			Console.WriteLine ("Hello World!");
-		}
+            //Variablen Deklaration
+            ushort bChoice;
+			
+            //Abfrage des gewünschten Pakettypuses
+            Console.WriteLine("Mind that until now nothing is working!");
+            Console.Write("Please choose from the given options: \n 1) Ping \n 2) ARP \n 3)...\n");
+            bChoice = Convert.ToUInt16(Console.ReadLine());
+
+            switch (bChoice)
+            {
+                case 1:
+                    Console.WriteLine("What is the destinationIp?");
+                    //Missing Code
+                    break;
+                case 2:
+                    //Missing Code
+                    break;
+            }
+        }
 	}
 }
